a27f2ab9fb737af43492bc930b8c1c7da75f5972
Muhammadzoir Mahmudov <kolen1470@gmail.com>
2024-04-01 22:06:34 +0500

    CUser app

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer(){
+  return(
+    <div className='footer'>
+      <div className="footer-container container">
+        <p>All Right Reserved. <a href="https://github.com/MakhmudovM">Makhmudov Mukhammazoir</a></p>
+      </div>
+   </div>
+  )
+}
+
+export default Footer
